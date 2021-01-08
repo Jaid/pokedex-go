b9f959e9c59b00f52424012c0dd8b402fcc08b56
Jaid <jaid.jsx@gmail.com>
2021-01-08 17:48:09 +0100

    Improved logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -3,6 +3,7 @@ import makeDir from "make-dir"
 import path from "path"
 import {getLatestVersionName} from "pokemongo-game-master"
 import readFileJson from "read-file-json"
+import statSizeText from "stat-size-text"
 import Stoppuhr from "stoppuhr"
 import yargs from "yargs"
 
@@ -18,13 +19,16 @@ async function job(argv) {
   const gamemasterFile = path.join(gamemasterFolder, "gamemaster.json5")
   await makeDir(gamemasterFolder)
   const gamemasterFileExists = await fsp.pathExists(gamemasterFile)
-  const gamemasterStoppuhr = new Stoppuhr
+  const stoppuhr = new Stoppuhr
   if (gamemasterFileExists) {
     gamemaster = await readFileJson(gamemasterFile)
-    console.log(`Loaded gamemaster in ${gamemasterStoppuhr.totalText()}`)
+    const sizeText = await statSizeText(gamemasterFile)
+    console.log(`Loaded gamemaster in ${stoppuhr.lapText()} (${sizeText})`)
   } else {
     gamemaster = await fetchGamemaster(gamemasterVersion)
     await fsp.outputJson5(gamemasterFile, gamemaster)
+    const sizeText = await statSizeText(gamemasterFile)
+    console.log(`Fetched gamemaster in ${stoppuhr.lapText()} (${sizeText})`)
   }
   const pokemonDataFolder = path.join(__dirname, "..", "dist", "pokemonData")
   const pokemonDataFile = path.join(pokemonDataFolder, "data.json5")
@@ -32,9 +36,13 @@ async function job(argv) {
   const pokemonDataFileExists = await fsp.pathExists(pokemonDataFile)
   if (pokemonDataFileExists) {
     pokemonData = await readFileJson(pokemonDataFile)
+    const sizeText = await statSizeText(pokemonDataFile)
+    console.log(`Loaded pokeapi data in ${stoppuhr.lapText()} (${sizeText})`)
   } else {
     pokemonData = await fetchPokemonData()
     await fsp.outputJson5(pokemonDataFile, pokemonData)
+    const sizeText = await statSizeText(pokemonDataFile)
+    console.log(`Fetched pokeapi data in ${stoppuhr.lapText()} (${sizeText})`)
   }
   console.log("MOIN")
 }
